aa4ef7ee54c51954e02e4a2098f7e2ba0bfb5c34
Prashikh Agrawal <prashikh.agrawal1@gmail.com>
2020-02-27 15:41:27 -0500

    Initial Commit

---
+++ Better Wireframe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Better Wireframe
+//
+//  Created by Prashikh Agrawal  on 27/02/20.
+//  Copyright © 2020 Prashikh Agrawal . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
